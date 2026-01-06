b49a426ddf58ec27d021553f58fed0e8c5b5d781
Jack Manning <eastarctica@gmail.com>
2026-01-05 22:58:18 -0500

    Add missing copyVitepressDoc to stonecutter

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -201,10 +201,39 @@ gradle.projectsEvaluated {
         }
     }
 
+    tasks.register("generateVitepressDoc", Javadoc::class.java) {
+        group = "documentation"
+        description = "Generates the vitepress documentation for the project"
+        source(documentationSources)
+        classpath = documentationClasspath
+        destinationDir = File(docsBuildDir, "vitepress")
+        options.doclet = "xyz.wagyourtail.doclet.mddoclet.Main"
+        options.docletpath = mutableListOf(docletJarFile)
+        (options as CoreJavadocOptions).addStringOption("v", project.version.toString())
+        (options as CoreJavadocOptions).addStringOption("mcv", mcVersion)
+        (options as StandardJavadocDocletOptions).links(
+            "https://docs.oracle.com/javase/8/docs/api/",
+            "https://www.javadoc.io/doc/org.slf4j/slf4j-api/1.7.30/",
+            "https://javadoc.io/doc/com.neovisionaries/nv-websocket-client/latest/"
+        )
+    }
+
+    tasks.register("copyVitepressDoc", Copy::class.java) {
+        group = "documentation"
+        description = "Copies the vitepress documentation to the build folder"
+        dependsOn("generateVitepressDoc")
+        from(rootProject.file("docs/vitepress"))
+        into(File(docsBuildDir, "vitepress"))
+        inputs.property("version", project.version.toString())
+        filesMatching("index.html") {
+            expand(mapOf("version" to project.version.toString()))
+        }
+    }
+
     tasks.register("createDistDocs", Copy::class.java) {
         group = "distribution"
         description = "Packages generated documentation into the dist directory"
-        dependsOn("prepareDist", "copyPyDoc", "copyTSDoc", "copyWebDoc")
+        dependsOn("prepareDist", "copyPyDoc", "copyTSDoc", "copyWebDoc", "copyVitepressDoc")
         from(docsBuildDir)
         into(distDirFile)
     }
